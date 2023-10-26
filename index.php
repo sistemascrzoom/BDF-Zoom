f3675b192ef128a8d4eb959629918e56cc6433ea
sistemascrzoom <sistemas.zoom@outlook.com>
2023-10-25 20:07:30 -0600

    s

--- index.php
+++ index.php
@@ -5,19 +5,6 @@
     <meta charset="utf-8">
     <title>Zoom - BDF</title>
   </head>
-  <link rel="stylesheet" href="estilo.css">
-	<link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-  <link href="vendorr/all.min.css" rel="stylesheet" type="text/css">
-	<!-- Bootstrap core JavaScript-->
-	<script src="vendorr/jquery/jquery.min.js"></script>
-	<script src="vendorr/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<!-- Core plugin JavaScript-->
-	<script src="vendorr/jquery-easing/jquery.easing.min.js"></script>
-	<script src="js/all.min.js"></script>
   <body>
     <div class="centro">
      <h3>Manejador de base de datos - BDF</h3>
