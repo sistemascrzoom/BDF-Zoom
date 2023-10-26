6d948f3c884b6c923e9351a0c26a8c7290ad85c2
sistemascrzoom <sistemas.zoom@outlook.com>
2023-10-25 20:04:22 -0600

    c

--- eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "conexion.php";
-    $id = $_GET['id'];
-    $query_delete = mysqli_query($conexion, "DELETE FROM master_latam WHERE ID = $id");
-    mysqli_close($conexion);
-    echo "El producto con el ID:" . " ". $id . " " . "fue eliminado correctamente";
-     header("location: productos.php");
-?>
